78faae926054e322754e3ec78a4d36fdbb984cf2
root <root@M.localdomain>
2021-05-08 22:20:40 +0800

    ui

--- ui/tree.go
+++ ui/tree.go
@@ -26,6 +26,10 @@ func NewTree(rootName string) *Tree {
 }
 
 func (t *Tree) onMouse(action tview.MouseAction, event *tcell.EventMouse) (tview.MouseAction, *tcell.EventMouse) {
+	switch action {
+	case tview.MouseScrollUp:
+	case tview.MouseScrollDown:
+	}
 	return action, event
 }
 
